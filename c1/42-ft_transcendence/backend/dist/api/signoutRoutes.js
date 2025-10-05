e8cb3ffc3076514182d2435b5cc6fb1f5e626017
theeunsu <theeunsu@gmail.com>
2025-10-05 02:03:00 +0200

    Revert "Merge pull request #4 from BekRobot99/import-commit1"
    This reverts commit 3ff8ae08c0ffda5054c4f31b6e4eb4f30715a3d7, reversing
    changes made to 2eac95aa11cbd644d8dd86c1f43dd9ac517e1cf8.

--- c1/42-ft_transcendence/backend/dist/api/signoutRoutes.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.default = signoutRoutes;
-async function signoutRoutes(app) {
-    app.post('/api/signout', async (request, reply) => {
-        reply.clearCookie('authToken', { path: '/' });
-        reply.status(200).send({ message: 'Signed out.' });
-    });
-}
--- c1/42-ft_transcendence/backend/src/api/signoutRoutes.ts
+++
@@ -1,8 +0,0 @@
-import { FastifyInstance } from 'fastify';
-
-export default async function signoutRoutes(app: FastifyInstance) {
-    app.post('/api/signout', async (request, reply) => {
-        reply.clearCookie('authToken', { path: '/' });
-        reply.status(200).send({ message: 'Signed out.' });
-    });
-}
